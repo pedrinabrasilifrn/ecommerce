4195b7392d97e7b0240882ebaa7b865c6bc9c3ac
Pedrina Celia Brasil <2131716@ifrn.local>
2023-11-16 11:39:39 -0300

    criacao do modelo do ususario

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Administrador(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    nome = models.CharField(max_length=255)
+    tel = models.CharField(max_length=15)
+    foto = models.ImageField(upload_to="perfil/")
